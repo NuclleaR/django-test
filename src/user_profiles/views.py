5e9501800da0f0eaf6c3476b58b393459e70ffc0
skorenuk <skorenuk@provectus.com>
2018-08-04 18:47:37 +0300

    Add search

--- src/user_profiles/views.py
+++ src/user_profiles/views.py
@@ -1,4 +1,5 @@
 from rest_framework.authentication import TokenAuthentication
+from rest_framework.filters import SearchFilter
 from rest_framework.viewsets import ModelViewSet
 
 from .permissions import UpdateOwnProfilePermission
@@ -11,3 +12,5 @@ class UserProfileViewSet(ModelViewSet):
     queryset = UserProfile.objects.all()
     authentication_classes = (TokenAuthentication,)
     permission_classes = (UpdateOwnProfilePermission,)
+    filter_backends = (SearchFilter,)
+    search_fields = ('email', 'username', 'first_name', 'last_name')
